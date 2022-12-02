9a96e4acc4e969becd78a7c56de981f6d8d3817f
PinoMacri <pinonegozio1@outlook.it>
2022-12-02 14:23:30 +0100

    Esercizio Consegnato

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("JS OK")
+////console.log("JS OK")
+
+/*
+1) Chiedi all'utente il suo nome
+2) Chiedi all'utente il suo cognome
+3) Chiedi all'utente il suo colore preferito
+4) Scrivi sulla pagina nomecognomecolorepreferito21
+*/
+
+
+// 1) Chiedi all'utente il suo nome
+const requestName = prompt("Qual'è il tuo nome?");
+////console.log (requestName)
+// 2) Chiedi all'utente il suo cognome
+const requestLastName = prompt("Qual'è il tuo cognome?");
+////console.log (requestLastName)
+// 3) Chiedi all'utente il suo colore preferito
+const requestColor = prompt("Qual'è il tuo colore preferito?");
+////console.log (requestColor)
+// 4) Scrivi sulla pagina nomecognomecolorepreferito21
+const passwordGenerator = document.getElementById('targetPassword').innerText = ` ${requestName}${requestLastName}${requestColor}21 `;
+//Non ho lasciato spazi su innerText perche dalla traccia "nomecognomecolorepreferito21" risulta tutto attaccato.
