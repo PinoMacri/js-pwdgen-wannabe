033de6ceea90bd1608971a851779d895989a48f0
PinoMacri <pinonegozio1@outlook.it>
2022-12-02 14:31:17 +0100

    Divido il punto 1 per dare più specificità.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,16 @@
 ////console.log("JS OK")
 
 /*
+1-A) Prendi l'elemento dal DOM
 1) Chiedi all'utente il suo nome
 2) Chiedi all'utente il suo cognome
 3) Chiedi all'utente il suo colore preferito
 4) Scrivi sulla pagina nomecognomecolorepreferito21
 */
 
-
+// 1-A) Prendi l'elemento dal DOM
+const passwordGenerator = document.getElementById('targetPassword');
+////console.log (passwordGenerator)
 // 1) Chiedi all'utente il suo nome
 const requestName = prompt("Qual'è il tuo nome?");
 ////console.log (requestName)
@@ -18,5 +21,5 @@ const requestLastName = prompt("Qual'è il tuo cognome?");
 const requestColor = prompt("Qual'è il tuo colore preferito?");
 ////console.log (requestColor)
 // 4) Scrivi sulla pagina nomecognomecolorepreferito21
-const passwordGenerator = document.getElementById('targetPassword').innerText = ` ${requestName}${requestLastName}${requestColor}21 `;
+passwordGenerator.innerText = ` ${requestName}${requestLastName}${requestColor}21 `;
 //Non ho lasciato spazi su innerText perche dalla traccia "nomecognomecolorepreferito21" risulta tutto attaccato.
